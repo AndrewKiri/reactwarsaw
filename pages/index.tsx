eeab6ee7a210fc2afe242c61196996de69457694
andrewkiri <andrewkirichok@gmail.com>
2020-11-10 23:25:13 +0100

    Test commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 const Home: NextPage = () => (
   <Layout>
     <h1>React Warsaw</h1>
-    <h2>Coming soon</h2>
+    <h2>Coming soon :)</h2>
   </Layout>
 )
 
